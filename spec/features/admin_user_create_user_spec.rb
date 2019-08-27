d52ae927cad7ff659ca765bcbb0d61d5b23c7de1
Firstname Lastname <e-mail address>
2019-08-27 00:32:19 -0700

    modify rspec test feature to only 1 test

--- spec/features/admin_user_create_user_spec.rb
+++ spec/features/admin_user_create_user_spec.rb
@@ -1,25 +1,9 @@
 require 'rails_helper'
 
 RSpec.feature "admin user create new user", type: :feature do
-  describe "homes#index action" do
-    it "should successfully show the page" do
-      visit '/'
-      expect(page).to have_http_status(:success)
-    end
-  end
-
   describe "admins action" do
-    it "should successfully login using admin account" do
-      admin = FactoryBot.create(:admin)
-      visit '/'
-      click_on 'Admin Sign In'
-      fill_in 'Email', :with => admin.email
-      fill_in 'Password', :with => admin.password
-      click_on 'Log in'
-      expect(page).to have_text("Create New User")
-    end
-
     it "should successfully create user account" do
+      #arrange
       admin = FactoryBot.create(:admin)
       visit '/'
       click_on 'Admin Sign In'
@@ -31,20 +15,10 @@ RSpec.feature "admin user create new user", type: :feature do
       fill_in 'user_email', :with => email
       fill_in 'user_password', :with => "password"
       fill_in 'user_password_confirmation', :with => "password"
+      #act
       click_button 'Sign up'
+      #assert
       expect(page).to have_content "Welcome! You have signed up successfully."
-    end
-  end
-
-  describe "users action" do
-    it "should successfully login using user account" do
-      user = FactoryBot.create(:user)
-      visit '/'
-      click_on 'User Sign In'
-      fill_in 'Email', :with => user.email
-      fill_in 'Password', :with => user.password
-      click_on 'Log in'
-      expect(page).to have_text("My Dashboard")
     end
   end
 end
